52517a428cbf87157ed7b6d4f244c8576761ab9e
GVS <jvasquez@gvscolombia.com>
2016-10-27 17:32:56 -0500

    se creo mainController y agrrego arreglo de ciudades

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,1 +1,27 @@
-var app = angular.module('ciudadesApp', []);
+var app = angular.module('ciudadesApp', ['ngRoute']);
+
+app.controller('mainController', function($scope){
+
+	$scope.cities = [
+		{name:'Cali', country:'Colombia'},
+		{name:'Bogotá', country:'Colombia'},
+		{name:'Medellín', country:'Colombia'},
+		{name:'Paris', country:'Francia'},
+		{name:'Londres', country:'Reino Unido'}
+	];
+
+	//agregar ciudades al arrglo
+	$scope.addCity = function(){
+
+		var newCity = {name: $scope.newCity, country: $scope.newCountry};
+
+		$scope.cities.push(newCity);
+
+		$scope.newCity = '';
+		$scope.newCountry = '';
+
+	};
+
+	//otro comentario
+
+});
